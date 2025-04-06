15d3c8d6203004ab2e9a143e72ac2f6c69c24cb0
png <jasona99@outlook.com>
2025-04-06 19:54:52 -0400

    Fix dev script and basic live preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,46 @@
 "use client";
 
-import { useEffect } from "react";
+import { SetStateAction, useState, useEffect, useRef, Dispatch } from "react";
 import { Camera } from "web-gphoto2";
 
-async function setupCamera() {
-  console.log("Clicked");
-  const camera = new Camera();
+async function setupCamera(camera: Camera, setCameraReady: Dispatch<SetStateAction<boolean>>) {
   await Camera.showPicker();
+  await new Promise(result => setTimeout(result, 1500));
   await camera.connect();
+  setCameraReady(true);
 }
 
+async function captureCamera(camera: Camera) {
+  await camera.captureImageAsFile();
+}
 
-function CameraPreview() {
-  return <canvas/>
+function CameraPreview(props: {
+  camera: Camera,
+  cameraReady: boolean,
+}) {
+  const canvasRef = useRef(null);
+  const animationFrame = useRef(null);
+
+  useEffect(() => {
+
+    async function renderFrame() {
+      if(!props.cameraReady) return;
+      props.camera.capturePreviewAsBlob()
+        .then((blob) => createImageBitmap(blob))
+        .then((bitmap) => {
+          const canvas = canvasRef.current;
+          if(!canvas || canvas == null) return;
+          const ctx = canvas.getContext('2d');
+          canvas.width = bitmap.width;
+          canvas.height = bitmap.height;
+          ctx.drawImage(bitmap, 0, 0);
+        })
+      animationFrame.current = requestAnimationFrame(renderFrame);
+      }
+    renderFrame();
+  })
+  console.log("Preview refresh", props.cameraReady);
+  return <canvas ref={canvasRef}/>
 }
 
 function IntroHeader() {
@@ -20,12 +48,15 @@ function IntroHeader() {
 }
 
 export default function Home() {
+  const [camera, setCamera] = useState(new Camera());
+  const [cameraReady, setCameraReady] = useState(false);
   return (
     <>
         <IntroHeader/>
         <text>What?</text>
-        <button onClick={setupCamera}>Connect</button>
-        <CameraPreview></CameraPreview>
+        <button onClick={() => setupCamera(camera, setCameraReady)}>Connect</button>
+        <button onClick={() => captureCamera(camera)}>Capture</button>
+        <CameraPreview camera={camera} cameraReady={cameraReady}></CameraPreview>
     </>
   );
 }
